3c45dd862729d56408ecf4535a4ceb36c0544625
Shrestha Mishra <smishra358@gatech.edu>
2024-04-25 17:43:29 -0400

    added skills

--- src/data/constants.js
+++ src/data/constants.js
@@ -20,13 +20,44 @@ export const Bio = {
         title: "Backend",
         skills: [
           {
+            name: ".NET Core",
+            image : "https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-plain-wordmark.svg"
+          },
+          {
+            name: "Spring Boot",
+            image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/unity/unity-original.svg"
+          },
+          {
+            name: "Unity",
+            image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/spring/spring-original.svg"
+          },
+          {
+            name: "Flask",
+            image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg"
+          },
+          {
             name: "Node Js",
             image: "https://nodejs.org/static/images/logo.svg",
+          },
+          {
+            name: "C#",
+            image:
+              "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg",
+          },
+          {
+            name: "Java",
+            image:
+              "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
           },
           {
             name: "Python",
             image:
               "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+          },
+          {
+            name: "Azure SQL",
+            image:
+              "https://raw.githubusercontent.com/devicons/devicon/master/icons/azuresqldatabase/azuresqldatabase-original.svg",
           },
           {
             name: "MySQL",
@@ -42,17 +73,7 @@ export const Bio = {
       },
     {
       title: "Frontend",
-      skills: [
-        {
-          name: "React Js",
-          image:
-            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
-        },
-        {
-          name: "Next Js",
-          image:
-            "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
-        },
+      skills: [     
         {
           name: "HTML",
           image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
@@ -68,6 +89,26 @@ export const Bio = {
             "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
         },
         {
+          name: "React Js",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+        },
+        {
+          name: "Svelte",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/svelte/svelte-original.svg",
+        },
+        {
+          name: "jQuery",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/jquery/jquery-original.svg",
+        },
+        {
+          name: "D3.js",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/d3js/d3js-original.svg",
+        },
+        {
           name: "Flutter",
           image:
             "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
@@ -75,28 +116,33 @@ export const Bio = {
       ],
     },
     {
-      title: "Android",
+      title: "Machine Learning",
       skills: [
         {
-          name: "Java",
+          name: "Jupyter",
           image:
-            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/jupyter/jupyter-original.svg"
         },
         {
-          name: "Kotlin",
+          name: "Keras",
           image:
-            "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/keras/keras-original.svg"
         },
         {
-          name: "XML",
+          name: "Pytorch",
           image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/pytorch/pytorch-original.svg"
         },
         {
-          name: "Android Studio",
+          name: "Numpy",
           image:
-            "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/numpy/numpy-original.svg"
         },
+        {
+          name: "TensorFlow",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/tensorflow/tensorflow-original.svg"
+        }
       ],
     },
     {
@@ -105,7 +151,7 @@ export const Bio = {
         {
           name: "Git",
           image:
-            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
         },
         {
           name: "GitHub",
@@ -113,34 +159,34 @@ export const Bio = {
             "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
         },
         {
-          name: "Docker",
+          name: "Azure",
           image:
-            "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/azure/azure-original.svg",
         },
         {
-          name: "Netlify",
+          name: "Jenkins",
           image:
-            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
-        },
-        {
-          name: "VS Code",
-          image:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/jenkins/jenkins-original.svg",
         },
         {
           name: "Postman",
           image:
-            "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/postman/postman-original.svg",
         },
         {
-          name: "Adobe XD",
+          name: "Confluence",
           image:
-            "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/confluence/confluence-original.svg",
         },
         {
           name: "Figma",
           image:
-            "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+        },
+        {
+          name: "Jira",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/jira/jira-original.svg",
         },
       ],
     },
